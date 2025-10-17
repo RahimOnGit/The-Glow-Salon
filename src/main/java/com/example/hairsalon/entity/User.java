05043b8ea4c8155e3976c3ba72f2f7a2ce3d0f3f
RahimOnGit <abdalraheem.saif7777@gmail.com>
2025-10-17 12:14:10 +0200

    user entity added

--- src/main/java/com/example/hairsalon/entity/User.java
+++ src/main/java/com/example/hairsalon/entity/User.java
@@ -1,8 +1,38 @@
 package com.example.hairsalon.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "Users")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role = "USER";
+
+    private Integer visitCount = 0;
 }
