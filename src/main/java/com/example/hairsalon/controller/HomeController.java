0d0bbf95e71014ded56df06bf030f9d959da058d
Khaled Ibrahim <khaled.abdo11@gmail.com>
2025-10-18 01:08:08 +0200

    pull HomeController

---
+++ src/main/java/com/example/hairsalon/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.hairsalon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";  // This serves your index.html
+    }
+}
